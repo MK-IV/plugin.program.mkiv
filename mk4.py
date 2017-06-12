20746eca6576136302d1b80ff1d1989aaef7bf9e
MK-IV <cameron.cantwell@gmail.com>
2017-06-12 11:52:28 -0300

    Create mk4.py

--- mk4.py
+++ mk4.py
@@ -11,7 +11,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License      sendclick exit or close()
+    You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import xbmc, xbmcaddon, xbmcgui, xbmcplugin,os,sys
